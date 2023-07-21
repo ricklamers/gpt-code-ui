773cf4fab3b763341f518502676835b3a0fcae91
Mathias Winkel <mathias.winkel@merckgroup.com>
2023-07-22 01:05:29 +0200

    PEP8 code styling, no functional change

--- gpt_code_ui/webapp/main.py
+++ gpt_code_ui/webapp/main.py
@@ -94,34 +94,34 @@ def inspect_file(filename: str) -> str:
 
 async def get_code(user_prompt, user_openai_key=None, model="gpt-3.5-turbo"):
 
-    prompt = f"""First, here is a history of what I asked you to do earlier. 
-    The actual prompt follows after ENDOFHISTORY. 
-    History:
-    {message_buffer.get_string()}
-    ENDOFHISTORY.
-    Write Python code, in a triple backtick Markdown code block, that does the following:
-    {user_prompt}
-    
-    Notes: 
-        First, think step by step what you want to do and write it down in English.
-        Then generate valid Python code in a code block 
-        Make sure all code is valid - it be run in a Jupyter Python 3 kernel environment. 
-        Define every variable before you use it.
-        For data munging, you can use 
-            'numpy', # numpy==1.24.3
-            'dateparser' #dateparser==1.1.8
-            'pandas', # matplotlib==1.5.3
-            'geopandas' # geopandas==0.13.2
-        For pdf extraction, you can use
-            'PyPDF2', # PyPDF2==3.0.1
-            'pdfminer', # pdfminer==20191125
-            'pdfplumber', # pdfplumber==0.9.0
-        For data visualization, you can use
-            'matplotlib', # matplotlib==3.7.1
-        Be sure to generate charts with matplotlib. If you need geographical charts, use geopandas with the geopandas.datasets module.
-        If the user has just uploaded a file, focus on the file that was most recently uploaded (and optionally all previously uploaded files)
-    
-    Teacher mode: if the code modifies or produces a file, at the end of the code block insert a print statement that prints a link to it as HTML string: <a href='/download?file=INSERT_FILENAME_HERE'>Download file</a>. Replace INSERT_FILENAME_HERE with the actual filename."""
+    prompt = f"""First, here is a history of what I asked you to do earlier.
+The actual prompt follows after ENDOFHISTORY.
+History:
+{message_buffer.get_string()}
+ENDOFHISTORY.
+Write Python code, in a triple backtick Markdown code block, that does the following:
+{user_prompt}
+
+Notes:
+    First, think step by step what you want to do and write it down in English.
+    Then generate valid Python code in a code block
+    Make sure all code is valid - it be run in a Jupyter Python 3 kernel environment.
+    Define every variable before you use it.
+    For data munging, you can use
+        'numpy', # numpy==1.24.3
+        'dateparser' #dateparser==1.1.8
+        'pandas', # matplotlib==1.5.3
+        'geopandas' # geopandas==0.13.2
+    For pdf extraction, you can use
+        'PyPDF2', # PyPDF2==3.0.1
+        'pdfminer', # pdfminer==20191125
+        'pdfplumber', # pdfplumber==0.9.0
+    For data visualization, you can use
+        'matplotlib', # matplotlib==3.7.1
+    Be sure to generate charts with matplotlib. If you need geographical charts, use geopandas with the geopandas.datasets module.
+    If the user has just uploaded a file, focus on the file that was most recently uploaded (and optionally all previously uploaded files)
+
+Teacher mode: if the code modifies or produces a file, at the end of the code block insert a print statement that prints a link to it as HTML string: <a href='/download?file=INSERT_FILENAME_HERE'>Download file</a>. Replace INSERT_FILENAME_HERE with the actual filename."""
 
     if user_openai_key:
         openai.api_key = user_openai_key

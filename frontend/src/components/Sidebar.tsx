e8f03a9a02b18b409116a0aef19212681a120f13
Mathias Winkel <mathias.winkel@merckgroup.com>
2023-10-20 12:19:40 +0200

    use material component for model selection dropdown

--- frontend/src/components/Sidebar.tsx
+++ frontend/src/components/Sidebar.tsx
@@ -1,6 +1,10 @@
 import AssistantIcon from '@mui/icons-material/Assistant';
 
 import "./Sidebar.css";
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
 
 export default function Sidebar(props: {
   models: Array<{ name: string; displayName: string }>;
@@ -29,19 +33,24 @@ export default function Sidebar(props: {
         </div>
         <div className="settings">
             <label className="header">Settings</label>
-            <label>Model</label>
-            <select
-            value={props.selectedModel}
-            onChange={(event) => props.onSelectModel(event.target.value)}
-            >
-            {props.models.map((model, index) => {
-                return (
-                <option key={index} value={model.name}>
-                    {model.displayName}
-                </option>
-                );
-            })}
-            </select>
+            <FormControl fullWidth>
+              <InputLabel id="model-select-label">Model</InputLabel>
+              <Select
+              labelId="model-select-label"
+              id="simple-select"
+              value={props.selectedModel}
+              label="Model"
+              onChange={(event: SelectChangeEvent) => props.onSelectModel(event.target.value as string)}
+              >
+              {props.models.map((model, index) => {
+                  return (
+                  <MenuItem key={index} value={model.name}>
+                      {model.displayName}
+                  </MenuItem>
+                  );
+              })}
+              </Select>
+            </FormControl>
         </div>
       </div>
     </>

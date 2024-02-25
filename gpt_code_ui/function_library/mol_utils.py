e3774a48de095acafb400b4271f08c2c3ad2abeb
Mathias Winkel <mathias.winkel@merckgroup.com>
2024-02-26 00:35:42 +0100

    add support for drawing multiple molecules, fix support for None return type
    partially cherry-picked from d01e9017a726dfc7d5f202f0100b21582c983dc8 in molecule-mate

---
+++ gpt_code_ui/function_library/mol_utils.py
@@ -0,0 +1,30 @@
+from typing import List
+
+from IPython.display import display
+from rdkit import Chem
+from rdkit.Chem import Draw
+
+
+def draw_smiles(smiles_list: List[str], legends: List[str] = None, mols_per_row: int = 5) -> None:
+    """Draw a list of SMILES strings.
+
+    Args:
+        smiles_list (List[str]): A list of SMILES strings.
+        legends (List[str], optional): A list of legend strings. Defaults to None.
+        mols_per_row (int, optional): Number of molecules per row. Defaults to 5.
+    """
+    if legends is not None:
+        display(
+            Draw.MolsToGridImage(
+                [Chem.MolFromSmiles(smiles) for smiles in smiles_list],
+                molsPerRow=mols_per_row,
+                legends=legends,
+            )
+        )
+    else:
+        display(
+            Draw.MolsToGridImage(
+                [Chem.MolFromSmiles(smiles) for smiles in smiles_list],
+                molsPerRow=mols_per_row,
+            )
+        )
